3291faa693dd467311a41cd8e91dd0541c836304
Praveen Kumar <praveen.kumar.paytm2019@gmail.com>
2023-07-17 17:34:38 +0530

    Set up order microservice using MySql db

---
+++ order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.ecommerce.orderservice.service;
+
+import com.ecommerce.orderservice.dto.OrderLineItemsDto;
+import com.ecommerce.orderservice.dto.OrderRequest;
+import com.ecommerce.orderservice.model.Order;
+import com.ecommerce.orderservice.model.OrderLineItems;
+import com.ecommerce.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+       orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItems.getPrice());
+        orderLineItems.setQuantity(orderLineItems.getQuantity());
+        orderLineItems.setSkuCode(orderLineItems.getSkuCode());
+        return orderLineItems;
+    }
+
+
+}
